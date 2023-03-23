b01206096d588ce57805d03974330bdefbd36be8
Sirajul islam <iamsiraj13@gmail.com>
2023-03-23 07:27:47 +0600

    slider functional

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import React from "react";
 // import ClientLogo from "./components/ClientLogo";
 // import Footer from "./components/Footer";
-// import HeroSlider from "./components/HeroSlider";
+import HeroSlider from "./components/HeroSlider";
 // import LatestProduct from "./components/LatestProduct";
 // import MySlider from "./components/MySlider";
 import OurSlide from "./components/OurSlide";
 // import PopularProduct from "./components/PopularProduct";
 // import ProductList from "./components/ProductList";
+import AOS from "aos";
+import "aos/dist/aos.css"; // You can also use <link> for styles
+// ..
 
 const App = () => {
+  AOS.init();
+  AOS.refresh();
   return (
     <>
       {/* <MySlider /> */}
-      <OurSlide />
-      {/* <HeroSlider /> */}
+      {/* <OurSlide /> */}
+      <HeroSlider />
       {/* <LatestProduct />
       <PopularProduct />
       <ProductList />
--- src/components/HeroSlider.jsx
+++ src/components/HeroSlider.jsx
@@ -1,82 +1,103 @@
-import React, { useState } from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
+import React, { useEffect, useRef, useState } from "react";
 
-import {   Thumbs } from "swiper";
-import "swiper/css/bundle";
 import headphone from "../assets/images/hero/headphone.png";
 import btncar from "../assets/images/hero/btncar.svg";
+import desktop from "../assets/images/hero/desktop.png";
+import mobile from "../assets/images/hero/phone.png";
+import watch from "../assets/images/hero/watch.png";
 import thumbheadphone from "../assets/images/hero/thumb-headphone.svg";
 import thumbdesktop from "../assets/images/hero/thumb-desktop.svg";
-import thumbwatch from "../assets/images/hero/thumb-mobile.svg";
+import thumbwatch from "../assets/images/hero/thumb-watch.svg";
 import thumbmobile from "../assets/images/hero/thumb-mobile.svg";
+import larrow from "../assets/images/hero/larrow.svg";
+import rarrow from "../assets/images/hero/rarrow.svg";
 // import thumbsearch from "../assets/images/hero/thumb-search.svg";
+import { BsChevronLeft, BsChevronRight, BsSlashLg } from "react-icons/bs";
 const HeroSlider = () => {
-  const [thumbsSwiper, setThumbsSwiper] = useState(null);
+  const slides = [
+    {
+      img: headphone,
+      title: "headphone",
+      desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis.",
+    },
+    {
+      img: desktop,
+      title: "smart tv",
+      desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis.",
+    },
+    {
+      img: watch,
+      title: "smart watch",
+      desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis.",
+    },
+    {
+      img: mobile,
+      title: "mobile",
+      desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis.",
+    },
+  ];
 
-  console.log(thumbsSwiper);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const goToPrev = () => {
+    const isFirstSlide = currentIndex === 0;
+    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  };
+  const goToNext = () => {
+    const isLastSlide = currentIndex === slides.length - 1;
+    const newIndex = isLastSlide ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  };
+  // const goToslide = (index) => {
+  //   setCurrentIndex(index);
+  // };
+  const thumb = (id) => {
+    setCurrentIndex(id);
+  };
+
+  useEffect(() => {
+    console.log("Hello");
+    setTimeout(() => {
+      goToNext();
+    }, 3000);
+  });
 
   return (
     <section className="slider-section">
       <div className="container">
-        <div>
-          <Swiper
-            style={{
-              "--swiper-navigation-color": "#fff",
-              "--swiper-pagination-color": "#fff",
-            }}
-            // onSwiper={setThumbsSwiper}
-            loop={true}
-            slidesPerView={4}
-            spaceBetween={10}
-            navigation={true}
-            thumbs={{ swiper: thumbsSwiper }}
-            modules={[Thumbs]}
-            className="mySwiper2"
-          >
-            <SwiperSlide>
-              <img src={thumbheadphone} alt="" />
-            </SwiperSlide>
-            <SwiperSlide>
-              <img src={thumbdesktop} alt="" />
-            </SwiperSlide>
-            <SwiperSlide>
-              <img src={thumbmobile} alt="" />
-            </SwiperSlide>
-            <SwiperSlide>
-              <img src={thumbwatch} alt="" />
-            </SwiperSlide>
-          </Swiper>
-        </div>
         <div className="slider-container">
-          <Swiper
-            spaceBetween={30}
-            centeredSlides={true}
-            thumbs={{ swiper: thumbsSwiper }}
-            autoplay={{
-              delay: 5000,
-              disableOnInteraction: false,
-            }}
-            pagination={{
-              clickable: false,
-            }}
-            navigation={false}
-            modules={[Thumbs]}
-            className="mySwiper"
-          >
-            <SwiperSlide>
+          <div className="slider-thumb">
+            <div className="thumb" onClick={() => thumb(0)}>
+              <img src={thumbheadphone} alt="" />
+            </div>
+            <div className="thumb" onClick={() => thumb(1)}>
+              <img src={thumbdesktop} alt="" />
+            </div>
+            <div className="thumb" onClick={() => thumb(2)}>
+              <img src={thumbwatch} alt="" />
+            </div>
+            <div className="thumb">
+              <img src={thumbmobile} alt="" onClick={() => thumb(3)} />
+            </div>
+          </div>
+          {/*---------- slider content-------- */}
+          <div className="slider">
+            {slides.map((slide, index) => (
               <div
-                className="slider-inner"
+                key={index}
+                className={`${
+                  currentIndex === index
+                    ? "slider-inner"
+                    : "d-none slider-inner"
+                } `}
                 style={{
-                  backgroundImage: `url(${headphone})`,
-                  backgroundPosition: "bottom center",
+                  backgroundImage: `url(${slides[currentIndex].img})`,
                 }}
               >
                 <div className="slider-content">
-                  <h2>headphone</h2>
-                  <p>
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                    Nobis.
-                  </p>
+                  <h2>{slide.title}</h2>
+                  <p>{slide.desc}</p>
                   <button>
                     buy now{" "}
                     <span>
@@ -85,54 +106,40 @@ const HeroSlider = () => {
                   </button>
                 </div>
               </div>
-            </SwiperSlide>
-            <SwiperSlide>
-              <div
-                className="slider-inner"
-                style={{
-                  backgroundImage: `url(${headphone})`,
-                  backgroundPosition: "bottom center",
-                }}
-              >
-                <div className="slider-content">
-                  <h2>headphone</h2>
-                  <p>
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                    Nobis.
-                  </p>
-                  <button>
-                    buy now{" "}
-                    <span>
-                      <img src={btncar} alt="" />
-                    </span>{" "}
-                  </button>
-                </div>
+            ))}
+          </div>
+          <div className="slider-right">
+            {/*-------- slider number -------- */}
+            <div className="slider-number">
+              <h4>
+                <span>{currentIndex + 1}</span>/ <span>{slides.length}</span>
+              </h4>
+            </div>
+            <div className="dot">
+              <h4>
+                <span>{currentIndex + 1}</span>/ <span>{slides.length}</span>
+              </h4>
+            </div>
+            {/* slider dot  */}
+            {/* <div className="dot">
+              {slides.map((slide, index) => (
+                <div className="dot-box"></div>
+              ))}
+            
+            </div> */}
+            {/*------- slider arrow -------- */}
+            <div className="slider-arrow">
+              <div className="left" onClick={goToPrev}>
+                <BsChevronLeft />
               </div>
-            </SwiperSlide>
-            <SwiperSlide>
-              <div
-                className="slider-inner"
-                style={{
-                  backgroundImage: `url(${headphone})`,
-                  backgroundPosition: "bottom center",
-                }}
-              >
-                <div className="slider-content">
-                  <h2>headphone</h2>
-                  <p>
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                    Nobis.
-                  </p>
-                  <button>
-                    buy now{" "}
-                    <span>
-                      <img src={btncar} alt="" />
-                    </span>{" "}
-                  </button>
-                </div>
+              <div className="slash">
+                <BsSlashLg />
               </div>
-            </SwiperSlide>
-          </Swiper>
+              <div className="right" onClick={goToNext}>
+                <BsChevronRight />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </section>
